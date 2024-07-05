1f11b2ddc28b333da573c86538e1599d880b3361
Bibek Lakra <bibeklakra1998@gmail.com>
2024-07-05 19:47:17 +0530

    db updated

--- hello/settings.py
+++ hello/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "sIplWMSXNQepJXhUfJRYcOrmKNWxvBKt",
+        "HOST": "monorail.proxy.rlwy.net",
+        "PORT": "24087",
     }
 }
 
@@ -128,8 +139,9 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 LOGIN_URL='/accounts/login'
 
-LOGIN_REDIRECT_URL='/tweet/'
-LOGOUT_REDIRECT_URL='/tweet/'
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL='/'
--- hello/urls.py
+++ hello/urls.py
@@ -22,6 +22,8 @@ from django.contrib.auth.urls import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tweet/', include('tweet.urls')),
+    path('', include('tweet.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] 
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
