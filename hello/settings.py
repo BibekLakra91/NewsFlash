921d9ad5036d0bedabb762b443c52a949b8d1276
Bibek Lakra <bibeklakra1998@gmail.com>
2024-07-05 21:47:31 +0530

    postgreSQL db update

--- hello/settings.py
+++ hello/settings.py
@@ -75,23 +75,23 @@ WSGI_APPLICATION = 'hello.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
-#     "default": {
-#         "ENGINE": "django.db.backends.postgresql",
-#         "NAME": "railway",
-#         "USER": "postgres",
-#         "PASSWORD": "sIplWMSXNQepJXhUfJRYcOrmKNWxvBKt",
-#         "HOST": "monorail.proxy.rlwy.net",
-#         "PORT": "24087",
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
 # }
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "railway",
+        "USER": "postgres",
+        "PASSWORD": "sIplWMSXNQepJXhUfJRYcOrmKNWxvBKt",
+        "HOST": "monorail.proxy.rlwy.net",
+        "PORT": "24087",
+    }
+}
 
 
 # Password validation
@@ -143,5 +143,5 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
 
 LOGIN_URL='/accounts/login'
 
-LOGIN_REDIRECT_URL='/tweet/'
-LOGOUT_REDIRECT_URL='/tweet/'
+LOGIN_REDIRECT_URL='/'
+LOGOUT_REDIRECT_URL='/'
