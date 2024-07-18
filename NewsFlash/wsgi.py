c2d4eba67569794d82d0bef812848e614c6ed145
Bibek Lakra <bibeklakra1998@gmail.com>
2024-07-19 03:23:28 +0530

    name NewsFlash

--- NewsFlash/wsgi.py
+++ NewsFlash/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for hello project.
+WSGI config for NewsFlash project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -10,7 +10,7 @@ https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
 import os
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hello.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'NewsFlash.settings')
 
 application = get_wsgi_application()
 app = application
