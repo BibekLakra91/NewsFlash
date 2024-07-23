25a2783a59d0a36301c80326d2dffde719c50e24
Bibek Lakra <bibeklakra1998@gmail.com>
2024-07-24 03:04:49 +0530

    project secret hidden

--- NewsFlash/settings.py
+++ NewsFlash/settings.py
@@ -19,13 +19,11 @@ load_dotenv()
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-a_$54x0#ya)kp=jp61h)brnvwm**wmv-()c^-*55#abotlkp_j'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
